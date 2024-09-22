478b1d661886798764b41b9c7a8a6b971c0f691e
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-09-22 21:47:10 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -34,8 +34,9 @@ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:m
 let down = await fg.yta(url)
 let downloadurl = down.dl_url
 
-//send audio message
+//send audio + document message
 await conn.sendMessage(from,{audio: {url:downloadurl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadurl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*_MADE BY RAHUL DEBNATH 💗🫶🏻_*"},{quoted:mek})
 
 
 
@@ -47,3 +48,49 @@ reply(`${e}`)
 }
 })
   
+//===========video-dl===========
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+☠️ *RAHUL MD VIDEO DOWNLOADER* ☠️
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+*_MADE BY RAHUL DEBNATH 💗🫶🏻_*
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadurl = down.dl_url
+
+//send video + document message
+await conn.sendMessage(from,{video: {url:downloadurl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadurl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*_MADE BY RAHUL DEBNATH 💗🫶🏻_*"},{quoted:mek})
+
+
+
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
