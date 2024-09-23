b83aaa48755427b5a004edd21e0f0ea449deaf4d
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-09-23 16:02:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const ownerNumber = ['919547261290']
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
 const sessdata = config.SESSION_ID
-const ironman = sessdata.replace('RAHUL-MD~', '');
+const ironman = sessdata.replace('RAHUL-MD~',);
 const filer = File.fromURL(`https://mega.nz/file/${ironman}`)
 filer.download((err, data) => {
 if(err) throw err
