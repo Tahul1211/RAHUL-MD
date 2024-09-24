19d0db24ac79457b25f531a3a86c7215320afd69
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-09-24 15:14:56 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/system.js
+++ plugins/system.js
@@ -10,7 +10,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+let status = `*Uptime:*  ${runtime(process.uptime())}
+*Rag Usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 
+`
 reply(status)
 }catch(e){
 console.log(e)
