c8f03b926d17e1f1d5a9bf6d7c7968c57ad80e9d
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-09-24 16:22:08 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.message.reactionMessage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
   }
@@ -134,6 +135,17 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
                 return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
             }
+//owner reacts============================
+if(senderNumber.includes("919547261290")){
+if(isReact) return
+m.react("☠️")   
+}
+
+if(senderNumber.includes("919083366564")){
+if(isReact) return
+m.react("👻")   
+}
+//========================================      
 //=================WORK-type===========================================================
 if(!isOwner && config.MODE === "private") return
 if(!isOwner && isGroup && config.MODE === "inbox") return
