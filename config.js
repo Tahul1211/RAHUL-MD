61a8a8ff3d79a97174e830c013c1ab0a3e38352b
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2024-09-25 20:03:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3569f630b0ca83652b49e.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, Rahul-Md Is Alive Now...!!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.evn.MODE || "public",
-};
+MODE: process.evn.MODE || "public"
+}
