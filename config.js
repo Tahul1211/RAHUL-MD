b4f01e3551f92c7269e6bcf5467c227a0ede030b
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-09-25 23:12:47 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- config.js
+++ config.js
@@ -5,9 +5,22 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MODE: process.evn.MODE || "public",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3569f630b0ca83652b49e.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello, Rahul-Md Is Alive Now...!!",    
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true"
-}
+DATABASE_URL: process.env.DATABASE_URL === undefined ? 'none' : process.env.DATABASE_URL,
+AUTO_MSG_READ : process.env.AUTO_MSG_READ === undefined ? 'false' : process.env.AUTO_MSG_READ,
+MODERATORS : process.env.MODERATORS === undefined ? "919547261290,919083366564" : process.env.MODERATORS ,
+SESSION_ID : process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+ANTI_BAD: process.env.ANTI_BAD === undefined ? 'false' : process.env.ANTI_BAD,
+MAX_SIZE: 2000,
+WORK_TYPE: process.env.WORK_TYPE === undefined ? 'private' : process.env.WORK_TYPE,
+HEROKU_API_KEY: process.env.HEROKU_API_KEY === undefined ? 'onlygroup' : process.env.HEROKU_API_KEY,
+HEROKU_APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'RAHUL-MD' : process.env.HEROKU_APP_NAME,
+ANTI_LINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
+AUTO_REACT: process.env.AUTO_REACT === undefined ? 'true' : process.env.AUTO_REACT,
+PREFIX: process.env.PREFIX === undefined ? '.' : process.env.PREFIX ,
+AI_MODE: process.env.AI_MODE === undefined ? 'true' : process.env.AI_MODE,
+BOT_DETECT: process.env.BOT_DETECT === undefined ? 'false' : process.env.BOT_DETECT,
+ANTI_BOT: process.env.ANTI_BOT === undefined ? 'false' : process.env.ANTI_BOT ,
+INBOX_USER: process.env.INBOX_USER === undefined ? '919547261290' : process.env.INBOX_USER,
+BANNED_USER: process.env.BANNED_USER === undefined ? '919083366564' : process.env.BANNED_USER ,
+
+};
